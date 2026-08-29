dc706d77f00a7d396ed4756106897a484be78317
Diogo Ribeiro <diogo_dj@hotmail.com>
2026-08-29 16:56:32 +0100

    Sort housing-tourism test imports

--- projects/portugal_housing_tourism/tests/test_indices.py
+++ projects/portugal_housing_tourism/tests/test_indices.py
@@ -6,8 +6,8 @@ import pytest
 from housing_tourism.indices import (
     external_housing_pressure_index,
     local_housing_decoupling_index,
-    tourist_housing_conversion_rate,
     tourism_intensity,
+    tourist_housing_conversion_rate,
 )
 
 

c5eb6b1f2de540ffccdfa6fd0fdc7996bc9411ca
Diogo Ribeiro <diogo_dj@hotmail.com>
2026-08-29 15:46:00 +0100

    Add housing tourism package exports

---
+++ projects/portugal_housing_tourism/src/housing_tourism/__init__.py
@@ -0,0 +1,15 @@
+"""Utilities for the Portugal housing-tourism project."""
+
+from .indices import (
+    external_housing_pressure_index,
+    local_housing_decoupling_index,
+    tourist_housing_conversion_rate,
+    tourism_intensity,
+)
+
+__all__ = [
+    "external_housing_pressure_index",
+    "local_housing_decoupling_index",
+    "tourist_housing_conversion_rate",
+    "tourism_intensity",
+]

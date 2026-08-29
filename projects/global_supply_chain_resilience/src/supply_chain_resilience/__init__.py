2a17626c6bd64bd4c665c83622a318041761aa60
Diogo Ribeiro <diogo_dj@hotmail.com>
2026-08-29 22:04:48 +0100

    Add supply chain package init

---
+++ projects/global_supply_chain_resilience/src/supply_chain_resilience/__init__.py
@@ -0,0 +1,5 @@
+"""Reusable analysis code for the global supply-chain resilience project."""
+
+from supply_chain_resilience.icio import ICIOTable, load_icio_csv, load_icio_zip
+
+__all__ = ["ICIOTable", "load_icio_csv", "load_icio_zip"]
